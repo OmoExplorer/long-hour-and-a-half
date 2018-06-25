7a96fbd6f68aa4479a22d509c439066ee21b1961
OmoExplorer <torso.webdriver@ya.ru>
2018-06-25 16:59:28 +0500

    Create UI class
    For having a base for developing different UIs such as graphical

---
+++ src/a_long_hour_and_a_half/ui.py
@@ -0,0 +1,15 @@
+class UI:
+    def __init__(self, game_state):
+        self._game_state = game_state
+
+    def update_character_stats(self):
+        pass
+
+    def update_actions(self):
+        pass
+
+    def show_main_menu(self):
+        pass
+
+    def get_action(self):
+        pass
