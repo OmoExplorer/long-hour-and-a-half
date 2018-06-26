231a33e7afd91f8976df6ac19d23f89417a89937
OmoExplorer <torso.webdriver@ya.ru>
2018-06-26 21:09:32 +0500

    Add crash handler

--- src/start.py
+++ src/start.py
@@ -1,3 +1,20 @@
-from a_long_hour_and_a_half import main
+from sys import exc_info
+from tkinter.messagebox import showerror
+from traceback import format_exc
 
-main()
+bar = '=' * 40
+
+try:
+    from a_long_hour_and_a_half import main
+
+    main()
+except Exception:
+    class_, message, tb = exc_info()
+
+    class_name = class_.__name__
+    showerror('A Long Hour and a Half: fatal error', "A fatal error has occured.\n\n" + bar + '\n'
+                                                     + format_exc() + bar + '\n\n'
+                                                     "Press Ctrl + C to copy this message.\n"
+                                                     "Please send this information to OmoExplorer @ omorashi.org"
+                                                     "\nor create an issue on GitHub:\n"
+                                                     ".\n\nI'm sorry for the inconvenience.")  # TODO
