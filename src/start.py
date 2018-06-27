a54572186a976c265d91931336f5aab3534a9d32
OmoExplorer <torso.webdriver@ya.ru>
2018-06-27 11:48:19 +0500

    Don't show traceback in crash dialog

--- src/start.py
+++ src/start.py
@@ -1,8 +1,6 @@
 from tkinter.messagebox import showerror
 from traceback import format_exc
 
-bar = '=' * 40
-
 # noinspection PyBroadException
 try:
     from a_long_hour_and_a_half import main
@@ -12,7 +10,7 @@ except Exception:
     with open('lhh_crash_info.txt', 'w+') as f:
         f.write(format_exc())
 
-    crash_text = "A fatal error has occured.\n\n" + bar + '\n' + format_exc() + bar + '\n\n' + \
+    crash_text = "A fatal error has occured.\n" \
                  "Crash info was written to 'lhh_crash_info.txt' in the same" \
                  "folder where the game is.\n" \
                  "Please send this information to OmoExplorer at omorashi.org" \
