89c4f6b6fd2f9b8564d8a7a994fc659703a42b2b
OmoExplorer <torso.webdriver@ya.ru>
2018-06-28 17:56:54 +0500

    Fix attribute

--- src/a_long_hour_and_a_half/actions.py
+++ src/a_long_hour_and_a_half/actions.py
@@ -65,7 +65,7 @@ ask_to_go_out = Action('Ask to go out', do_ask_to_go_out)
 
 
 def do_hold(state):
-    state.character.bladder.sphincter.power += 10
+    state.character.sphincter.power += 10
     if chance(12):
         state.classmates.notice_holding()
 
