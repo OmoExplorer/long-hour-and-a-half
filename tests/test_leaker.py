a797f294bfb0822f6438486d3bccc6251cdd835d
OmoExplorer <torso.webdriver@ya.ru>
2018-06-30 21:48:05 +0500

    Add leak volumes tests for Leaker

--- tests/test_leaker.py
+++ tests/test_leaker.py
@@ -14,3 +14,15 @@ class TestLeaker(unittest.TestCase):
         for i in range(int(self.bladder.maximal_urine)):
             self.bladder.urine = i
             self.assertEqual(self.leaker.leak_chance, (self.bladder.urine - self.leaker._fullness_threshold) / 2)
+
+    def test_minimal_leak_volume(self):
+        for i in range(int(self.bladder.maximal_urine)):
+            self.bladder.urine = i
+            self.assertEqual(self.leaker.minimal_leak_volume,
+                             max(1, self.bladder.urine ** 2 // 35000 - 23))
+
+    def test_maximal_leak_volume(self):
+        for i in range(int(self.bladder.maximal_urine)):
+            self.bladder.urine = i
+            self.assertEqual(self.leaker.maximal_leak_volume,
+                             max(0, self.bladder.urine ** 2 // 20000 - 32))
